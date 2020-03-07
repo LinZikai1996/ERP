06f0a7b8a3709f35d0b62e1ad726b2f81d325ebb
Lin <elegantrabbit@163.com>
2020-03-07 17:05:55 +0800

    spring and spring mvc

--- src/main/java/com/ERP/domain/User.java
+++ src/main/java/com/ERP/domain/User.java
@@ -1,6 +1,42 @@
 package com.ERP.domain;
 
+import java.util.List;
+
 public class User {
     private String id;
     private String userName;
+    private List<Order> orderList;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public List<Order> getOrderList() {
+        return orderList;
+    }
+
+    public void setOrderList(List<Order> orderList) {
+        this.orderList = orderList;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", userName='" + userName + '\'' +
+                ", orderList=" + orderList +
+                '}';
+    }
 }
